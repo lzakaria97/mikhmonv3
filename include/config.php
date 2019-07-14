113bbec9a7287bc073aefae59a4dd172582e45ee
lzakaria97 <33273912+lzakaria97@users.noreply.github.com>
2019-07-14 09:27:38 +0700

    Add files via upload

---
+++ include/config.php
@@ -0,0 +1,5 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<admin','mikhmon>|>mZWfoZ8=');
+
+$data['wifirt03'] = array ('1'=>'wifirt03!192.168.14.1','wifirt03@|@lutfi','wifirt03#|#mp2TrWNiaWo=','wifirt03%WiFi RT03','wifirt03^hotspot.rt3.net','wifirt03&Rp','wifirt03*10','wifirt03(1','wifirt03)0','wifirt03=0','wifirt03@!@enable');
